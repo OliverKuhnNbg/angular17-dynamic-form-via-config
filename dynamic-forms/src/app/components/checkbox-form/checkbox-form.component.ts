1ad947aeae86a9363c28d279f356fc69d4b7fe83
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-04-28 00:51:02 +0200

    improve checkbox logic - remove logs partly

--- dynamic-forms/src/app/components/checkbox-form/checkbox-form.component.ts
+++ dynamic-forms/src/app/components/checkbox-form/checkbox-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormConfigService } from '../../services/form-config.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,17 +13,16 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrl: './checkbox-form.component.scss'
 })
 export class CheckboxFormComponent {
-  url: string= 'assets/form-configs/responsibility.config.json'
-  //serviceFormCategorys: FormGroup;
-  serviceFormData: any
+  url: string= 'assets/form-configs/responsibility.config.json';
+  serviceFormCategorys: FormGroup = new FormGroup({});
+  serviceFormData: any;
+  serviceNames: string[] = [''];
+  serviceTypesNames: string[] = [''];
+
+  inputType: string = 'checkbox';
 
   constructor(private fb: FormBuilder, private formConfigService: FormConfigService) {
-    console.log('--------------------\n\n\tconstructor\n-----------------------\n\n')
-    // this.serviceFormCategorys = this.fb.group({
-    //   name: ['', Validators.required],
-    //   email: ['', [Validators.required, Validators.email]]
-    // });
-    
+    console.log('--------------------\n\n\tconstructor\n-----------------------\n\n');
   }
   
   ngOnInit(): void {
@@ -31,24 +30,39 @@ export class CheckboxFormComponent {
     this.formConfigService.getResponsibilityFormConfig().subscribe((data:any) => {
       this.serviceFormData = data;
       if(data!=undefined) this.createForm();
-    })
+    });
   }
 
   private createForm() {
     console.log('--------------------\n\n\tcreateForm(serviceFormData: any)\n-----------------------\n\n');
     console.log('DATA1:\n------------------',this.serviceFormData);
-    let serviceNames = this.getServiceNames();
-    console.log('DATA3:\n------------------',serviceNames);
+    this.serviceNames = this.getServiceNames(this.serviceFormData);
+    this.serviceTypesNames = this.getServiceNames(this.serviceFormData.water.connection);
+    console.log('DATA3:\n------------------',this.serviceNames);
+    console.log('DATA4:\n------------------',this.serviceTypesNames);
+
+    const mainFormGroup = this.fb.group({});
+    const serviceCategoryFormGroup = this.fb.group({});
+    const serviceTypeFormGroup = this.fb.group({});
+    console.log('\n\nDATA5:\n------------------',serviceCategoryFormGroup);
+    this.serviceNames.map((name) => {
+      serviceCategoryFormGroup.addControl(name, new FormControl(false));
+    });
+
+    this.serviceFormCategorys = serviceCategoryFormGroup;
+    console.log('\n\nDATA6:\n------------------', serviceCategoryFormGroup);
   }
-  private getServiceNames() {
-    console.log('--------------------\n\n\tgetServiceNames(dataObject: any)\n-----------------------\n\n');
-    console.log('DATA2:\n------------------',this.serviceFormData);
+  private getServiceNames(serviceFormData: any) {
     let names:string[] = []
-    Object.entries(this.serviceFormData!).map(([key]) => {
-      console.log(key)
+    Object.entries(serviceFormData!).map(([key]) => {
       names.push(key);
     });
     return names;
+  }
+
+  onSubmit() {
+    console.log('\n\nDATA7:\n------------------', this.serviceFormCategorys.controls);
+    throw new Error('Method not implemented.');
   }
   
 }
