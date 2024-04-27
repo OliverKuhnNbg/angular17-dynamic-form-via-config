ed8f7f7673324fd3d0255a9d702d2259015daad7
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-04-27 11:48:51 +0200

    init checkbox componet

---
+++ dynamic-forms/src/app/components/checkbox-form/checkbox-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkbox-form.component.html',
+  styleUrl: './checkbox-form.component.scss'
+})
+export class CheckboxFormComponent {
+
+}
