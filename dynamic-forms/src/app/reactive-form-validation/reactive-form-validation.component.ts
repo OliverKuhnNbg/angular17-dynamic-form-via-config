dc64e7c23e88e99b5e01a1d85e1f9fe0d9a30c3a
OliverKuhnNbg <oliver.o.kuhn@googlemail.com>
2024-04-28 20:48:57 +0200

    create - userForm: FormGroup

--- dynamic-forms/src/app/reactive-form-validation/reactive-form-validation.component.ts
+++ dynamic-forms/src/app/reactive-form-validation/reactive-form-validation.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-validation',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-form-validation.component.html',
   styleUrl: './reactive-form-validation.component.scss'
 })
 export class ReactiveFormValidationComponent {
+  userForm: FormGroup;
 
+  constructor(){
+    this.userForm = new FormGroup({
+      firsName: new FormControl(''),
+      lastName: new FormControl(''),
+      userName: new FormControl(''),
+      city: new FormControl(''),
+      zip: new FormControl(''),
+    })
+  }
 }
